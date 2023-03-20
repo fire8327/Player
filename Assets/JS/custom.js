af0fd3882b1240575aba97ccf737d9eddd1da95d
fire8327 <fire83274@gmail.com>
2023-03-20 13:45:22 +0300

    create modal

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,1 +1,29 @@
 /*jshint esnext: true */
+$(".param").each(function (index, el){
+    $(el).click(function() {        
+        $("#modal").addClass("z-50");
+        $("#overlay").addClass("z-40");
+        setTimeout(function(){
+            $("#modal").removeClass("opacity-0");
+            $("#overlay").removeClass("opacity-0");
+        }, 200);    
+    });
+});
+
+$("#close").click(function() {
+    $("#modal").addClass("opacity-0");
+    $("#overlay").addClass("opacity-0");
+    setTimeout(function(){
+        $("#modal").removeClass("z-50");
+        $("#overlay").removeClass("z-40");
+    }, 300);    
+});
+
+$("#overlay").click(function() {
+    $("#modal").addClass("opacity-0");
+    $("#overlay").addClass("opacity-0");
+    setTimeout(function(){
+        $("#modal").removeClass("z-50");
+        $("#overlay").removeClass("z-40");
+    }, 300); 
+});
